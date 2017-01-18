495e943ebe209f8b829258ca8870221689f89c1f
Thibaut Remy <thibaut.remy@fyndiq.com>
2017-01-18 14:19:42 +0100

    Add APIMiddleware

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  APIMiddleware: ({ apps }) => {
+    return (req, res, next) => {
+      const appName = req.body.app
+      if (!(appName in apps)) {
+        res.status(404).json({ error: `Unknown app : ${req.body.app}` })
+      } else {
+        apps[appName](req)
+        .then(response => {
+          res.json({
+            response,
+          })
+        })
+        .catch(err => {
+          next(err)
+        })
+      }
+    }
+  },
+}
